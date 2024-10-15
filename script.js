8f35ee9f75712b1d145fc004072886c43732136c
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2024-10-15 20:36:43 +0530

    Create a function 'getHumanInput'
    I created a function to get human input. A variable 'humanChoice' will keep taking input until the value is equal to rock,paper or scissor.
    
    The user will be asked for input via prompt() function. The value will stored in 'humanChoice'.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice () {
     let compChoice = Math.floor(Math.random() * 100) + 1;
-    
+
     if(compChoice <= 33 && compChoice >= 1) {
         return "rock";
     }
@@ -9,5 +9,16 @@ function getComputerChoice () {
     }
     else {
         return "paper";
+    }
+}
+
+function getHumanChoice () {
+    let humanChoice;
+    while (true) {
+        humanChoice = prompt("What's your choice : ", 'rock');
+        if(humanChoice === "rock" ||
+           humanChoice === "paper" ||
+           humanChoice === "scissor"
+        ) break;
     }
 }
